62875228e6713680253759279d61dfedeb8335b7
CalissaT <calissat@gmail.com>
2024-08-20 19:55:32 -0700

    More menu adjustments

--- skiJumper/Assets/Scripts/CrashDetector.cs
+++ skiJumper/Assets/Scripts/CrashDetector.cs
@@ -2,15 +2,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
 
 public class CrashDetector : MonoBehaviour
 {
+    [SerializeField] ParticleSystem crash;
+    [SerializeField] GameObject panel;
+
     //temp for player fail
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Ground")
         {
-            SceneManager.LoadScene(1);
+            crash.Play();
+            Invoke("GameOver", 0.15f);
         }
     }
+
+    public void GameOver()
+    {
+        Time.timeScale = 0;
+        panel.SetActive(true);
+    }
+
+    public void Retry()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(1);
+    }
+
+    public void MainMenu()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(0);
+    }
+
+
 }
--- skiJumper/Assets/Scripts/Finish.cs
+++ skiJumper/Assets/Scripts/Finish.cs
@@ -5,11 +5,14 @@ using UnityEngine.SceneManagement;
 
 public class Finish : MonoBehaviour
 {
+    [SerializeField] ParticleSystem finishLine;
     //temp for game finish
     private void OnTriggerEnter2D(Collider2D other)
     {
+
         if (other.tag == "Player")
         {
+            finishLine.Play();
             SceneManager.LoadScene(1);
         }
     }
